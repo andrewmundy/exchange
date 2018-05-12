fa1feb4f694157d15ccaab76f98aaab0bdc857a5
andrewmundy <andrewmundy@gmail.com>
2018-05-12 08:39:31 +0700

    server

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
-// app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('server started ' + port);
+
