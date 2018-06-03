1bd07cf8d05c97500518c5d6d577ade6d5ed6727
andrewmundy <andrewmundy@gmail.com>
2018-06-02 23:25:32 -0700

    secured cross origin posts

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <div>
+      <!-- <img class="bg" src="./bg2.svg">
+      <img class="bg2" src="./bg1.svg"> -->
       <div id="container">
-        <div class="contents">
+        <div class="info">
           <div class="inputs">
-          {{total()}}
-
-            <input
-              v-bind:placeholder="this.country"
-              v-model="us"
-            >
-            <input
-              v-model="exchange"
-              v-on:input="total"            
-            >
+              <input 
+                type="number"
+                min="0"
+                step="1"
+                v-model="fromValue"    
+              >
+              <select v-model="fromCountry">
+                <option>{{fromCountry}}</option>
+                <option>USD</option>
+                <option>THB</option>
+                <option>LKP</option>
+              </select>
           </div>
-          <div id="output"></div>
-          <!-- <div class="bottom" id="output">
-            <div>exchange</div>
-            <div>stuff</div>
-          </div> -->
+
+          <div class="contents">
+            <div class="inputs">
+              <input
+                type="number"
+                min="0"
+                step="any"
+                v-model="toValue"
+              >
+              <select v-model="toCountry">
+                <option>THB</option>
+                <option>USD</option>
+                <option>LAK</option>
+              </select>
+
+            </div>
+          
+          </div>
+
         </div>
+            <!-- <span>{{fromCountryFlag}}</span> -->
+          <div id="map"></div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -31,11 +49,18 @@
     name: 'app',
     data: function(){
 			return{
+        fromValue:1,
+        fromCountry:"USD",
+        fromCountryName:"",
+        fromCountryFlag:"",
+        fromCountryCode:"",
+        toValue:0,
+        toCountry:"THB",
+        toCountryName:"",
+        toCountryFlag:"",
+        toCountryCode:"",
 				value: 'hello',
-				us:"",
-				rate:"",
-        exchange:'',
-        country:"THB"
+        rate:100,
 			}
     },
     mounted:function(){
@@ -44,37 +69,56 @@
       this.assignCountry()
       // this.total()
     },
+    updated:function(){
+      // this.money()
+    },
+    watch:{
+      fromCountry(){
+        this.money()
+      },
+      toCountry(){
+        this.money()
+      }
+    },
     methods:{
       assignCountry: function(){
         this.country = "THB"
       },
       money: function (){
-        this.us = "$" + this.us
-      },
-      total: function(){
-        this.exchange = "$" + Math.round(this.us * this.rate) + " 🇺🇸"
+        this.from = "$" + this.from
       },
       money: function(){
-        let url = "https://data.fixer.io/api/latest?access_key=c98b600783e240ffe856deaaa0747493"
+        let from = this.fromCountry
+        let to = this.toCountry
+        let convert = "https://free.currencyconverterapi.com/api/v5/convert?q="+from+"_"+to+"&compact=ultra"
+        let countries = "https://free.currencyconverterapi.com/api/v5/countries"
         let that = this
-        fetch(url)
+        fetch(countries)
           .then((resp) => resp.json())
           .then(function(data){
-            that.rate = data.rates[that.country]*0.001
+            that.fromCountry = data.results[that.fromCountryCode].currencyId
+            console.log(that.fromCountry)
           })
+        fetch(convert)
+          .then((resp) => resp.json())
+          .then(function(data){
+            that.rate = data[from+"_"+to]
+            that.toValue = that.fromValue * that.rate
+          })
+        that.toValue = that.fromValue * that.rate.toFixed(2)
       },
       location: function(){
+        let that = this
         const ip = '134.201.250.155'
         const access_key = 'e1f8abf46f10cca826e52a483d711dac'
-        const url = 'https://api.ipstack.com/' + ip + '?access_key=' + access_key
-
+        const url = 'http://api.ipstack.com/' + ip + '?access_key=' + access_key
         if ("geolocation" in navigator) {
           navigator.geolocation.getCurrentPosition(function(position) {
           let latitude = position.coords.latitude
           let longitude = position.coords.longitude
           var img = new Image();
-          img.src = "https://maps.googleapis.com/maps/api/js?key=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=true";
-          output.appendChild(img);
+          img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=400x120&sensor=false";
+          map.appendChild(img);
         })
         } else {
           console.log("oops")
@@ -83,7 +127,11 @@
         fetch(url)
           .then((resp) => resp.json())
           .then(function(data){
-            console.log(data.country_name)
+            that.fromCountryName = data.country_name
+            that.fromCountryCode = data.country_code
+            that.fromCountryFlag = data.location.country_flag_emoji
+
+            console.log(data)
           })
       }
     }
@@ -91,119 +139,72 @@
 </script>
 
 <!-- CSS libraries -->
-<style src="normalize.css/normalize.css"></style>
+<style type="text/scss">
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  background-image: linear-gradient(to top, #00c6fb 0%, #005bea 100%);
+  height:100vh;
+}
+.info{
+  padding:2rem;
+}
 
-<!-- Global CSS -->
-<style>
-  .bottom{
-    background:white;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width:100vw;
-    position:fixed;
-    bottom:0;
-  }
-  .bottom *{
-    background: white;
-    color: black;
-    width: 25%;
-    padding: 1rem 0;
-    box-shadow: 0px 10px 20px #0000001a;
-    border-radius: 10px;
-  }
-  img{
-    /* border-style:solid; */
-    border-radius:100rem;
-    padding:1rem;
-  }
-  img #output{
-  }
-  h1{
-    padding:1rem;
-    color:white !important;
-    font-size:3rem;
-  }
-  body{
-    font-family:sans-serif;
-    color:white;
-    background-image: linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%);
-    height:100vh;
-  }
-  input{
-    font-size:3rem;
-    border:none;
-    background:rgba(255,255,255,0.2);
-    padding:1rem;
-    margin:1rem;
-    border-radius:5px;
-    width:20%;
-    color:white;
-  }
-  input::placeholder{
-      color:white;
-  }
-  .inputs{
-    display:flex;
-    justify-content:center;
-  }
-  .contents{
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    flex-direction:column;
-  }
-  #container{
-    display:flex;
-    justify-content:center;
-  }
-  code {
-    font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
-    font-size: 0.9em;
-    white-space: pre-wrap;
-    color: #2c3e50;
-  }
+.inputs input{
+  width:auto;
+  font-size: 1rem;
+}
+select{
+  font-size:1rem;
+}
+.legend div{
+  margin:auto;
+  width: 50%;
+  display:flex;
+  justify-content: space-between;
+  color:grey;
+}
+.bg2{
+  opacity: 0.4;
+  right: 0;
+  top: -800px;
+  width: 60vw;
+  position: fixed;
+  z-index: 0;
+}
+.bg{
+  opacity:0.4;
+  left: 0;
+  top: -100px;
+  width: 60vw;
+  position: fixed;
+  z-index: 0;
+}
+#map{
+  height: 120px;
+}
+#map > img{
+  border-radius: 0 0 10px 10px;
+  margin-top:1rem;
 
-  code::before, code::after {
-    content: '`';
-  }
+}
+.inputs{
+text-align: left;
+  padding:0.2rem;
+}
+#app{
+  display:flex;
+  justify-content: center;
+  align-content: center;
+}
+#container{
+  width:400px;
+  text-align: center;
+  background: white;
+  box-shadow: 0px 20px 70px #0006;
+  /* padding: 3rem 2rem 0 2rem; */
+  border-radius: 0 0 10px 10px;
+  position: fixed;
+  top:0;
+}
 </style>
 
-<!-- Scoped component css -->
-<!-- It only affect current component -->
-<style scoped>
-  #app {
-    text-align: center;
-  }
-
-  #app h1 {
-    color: #2c3e50;
-    font-weight: 300;
-    margin: 0;
-  }
-
-  .banner {
-    height: 200px;
-    background-color: #f6f6f6;
-    padding: 50px 10px;
-  }
-
-  .bottom {
-    padding: 80px 10px;
-    font-size: 24px;
-    font-weight: 300;
-  }
-
-  .fade {
-    font-size: 14px;
-  }
-
-  .logo {
-    animation: spin 4s 1s infinite linear
-  }
-
-  @keyframes spin {
-    from {transform:rotate(0deg);}
-    to {transform:rotate(360deg);}
-  }
-</style>
